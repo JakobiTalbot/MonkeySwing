7114a9def192c0a0682c623353b160a090c2e71c
JakobiTalbot <jakobi.talbot@gmail.com>
2024-07-08 15:50:03 +1000

    - Tweaked movement
    - Added game manager with fail state
    - Added TMP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameOverText;
+
+    static public GameManager instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void GameOver(int score)
+    {
+        gameOverText.SetActive(true);
+        gameOverText.GetComponent<TextMeshProUGUI>().text += score.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,31 +11,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float perfectSwingSpeedGain = 5f;
     [SerializeField]
-    private Vector3 startDirection = new Vector3(1f, 0f, 0f);
-    [SerializeField]
-    private float startSpeed = 1f;
+    private Vector3 startVelocity = new Vector3(10f, 0f, 0f);
     [SerializeField]
     float gravity = -9.8f;
-    [SerializeField]
-    float swingAcceleration = 1f;
 
     private LineRenderer lineRenderer;
 
     private bool isSwinging = false;
     private Vector3 tetherPoint;
-    private float ropeLength;
-    private Vector3 direction;
-    float speed;
+    private Vector3 velocity;
+    private bool gameOver = false;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        direction = startDirection;
-        speed = startSpeed;
+        velocity = startVelocity;
     }
 
     private void Update()
     {
+        if (gameOver)
+            return;
+
         // start/stop swinging
         if (Input.GetKeyDown(KeyCode.Space))
             StartSwing();
@@ -45,26 +42,35 @@ public class PlayerController : MonoBehaviour
         // swing calculations
         if (isSwinging)
             Swing();
-        else // only do gravity if not swinging TODO: do gravity if not at max rope length ?
-            direction.y += gravity * Time.deltaTime;
+        else // only do gravity if not swinging
+            velocity.y += gravity * Time.deltaTime;
 
         // move
-        transform.position += direction * speed * Time.deltaTime;
-        // face player towards direction they're moving
-        transform.LookAt(transform.position + direction);
+        transform.position += velocity * Time.deltaTime;
+        // rotate forwards
+        transform.LookAt(transform.position + velocity.normalized);
+    }
+
+    private void LateUpdate()
+    {
+        // set rope points if swinging
+        if (isSwinging)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, tetherPoint);
+        }
 
     }
 
     private void StartSwing()
     {
-        // find grapple point
+        // can probably do this without raycasting, not sure if worth it though
+        // raycast for grapple point
         RaycastHit hit;
-        // get raycast direction upwards of player velocity direction
-        Vector3 dir = Vector3.Cross(direction, Vector3.back);
-        dir.x = Mathf.Clamp(dir.x, 0f, 1f);
+        Vector3 dir = Vector3.Cross(velocity.normalized, Vector3.back); // get raycast direction upwards of player velocity direction
+        dir.x = Mathf.Max(dir.x, 0f); // don't let player grapple behind them
         Physics.Raycast(transform.position, dir, out hit, 100f, 1 << 6); // ceiling bounds have layer 6
         tetherPoint = hit.point;
-        ropeLength = hit.distance; // set max rope length to length between player and grapple point
 
         lineRenderer.enabled = true; // show rope
         isSwinging = true;
@@ -76,25 +82,24 @@ public class PlayerController : MonoBehaviour
         lineRenderer.enabled = false;
 
         // TODO: implement perfect swing
-        float perfectSwingFactor = Vector3.Dot(transform.forward, perfectSwingDirection);
+        //float perfectSwingFactor = Vector3.Dot(transform.forward, perfectSwingDirection);
         //speed *= Mathf.Max(perfectSwingFactor, 1f) * perfectSwingSpeedGain;
-
-        Debug.Log(perfectSwingFactor);
     }
 
     private void Swing()
     {
-        // set rope points
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, tetherPoint);
-
-        // TODO: accelerate slower / deccelerate past certain point ?
-        // accelerate speed while swinging
-        //speed += swingAcceleration * Time.deltaTime;
-        //Debug.Log(speed);
-
-        // set swing direction
+        // set velocity in swing direction
         Vector3 tetherPointDir = (tetherPoint - transform.position);
-        direction = Vector3.Cross(Vector3.back, tetherPointDir).normalized;
+        velocity = Vector3.Cross(Vector3.back, tetherPointDir).normalized * velocity.magnitude;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // WIP fail state, quick fix
+        if (other.GetComponentInParent<BackgroundMover>())
+        {
+            GameManager.instance.GameOver((int)transform.position.x);
+            gameOver = true;
+        }
     }
 }
